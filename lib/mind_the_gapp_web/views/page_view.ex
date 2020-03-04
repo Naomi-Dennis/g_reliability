0341572a471bc3761408ecaa2c9cbf2506b77bf8
Naomi Dennis <naomi@8thlight.com>
2020-03-04 08:43:36 -0500

    Fix

--- lib/mind_the_gapp_web/views/page_view.ex
+++ lib/mind_the_gapp_web/views/page_view.ex
@@ -2,6 +2,6 @@ defmodule MindTheGappWeb.PageView do
   use MindTheGappWeb, :view
 
   def render("index.json", %{data: data}) do
-    %{some_data_more: data}
+    %{some_data_more: data, yet_even_more_data: 1000000}
   end
 end
