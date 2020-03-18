2495f7ba17f2c0766ed1c4e45e7ba415e4240d03
Naomi Dennis <naomi@8thlight.com>
2020-03-18 10:18:20 -0400

    Remove default page_view files

--- lib/mind_the_gapp_web/controllers/page_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule MindTheGappWeb.PageController do
-  use MindTheGappWeb, :controller
-
-  def index(conn, _params) do
-    data = %{some_more_data: 100}
-    render(conn, "index.json",data: data)
-  end
-end
--- test/mind_the_gapp_web/controllers/page_controller_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule MindTheGappWeb.PageControllerTest do
-  use MindTheGappWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    assert true
-  end
-end
