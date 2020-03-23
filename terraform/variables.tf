555322b66717cabe7ce3ad98a31169b4b4dccacc
Naomi Dennis <naomi@8thlight.com>
2020-03-23 11:04:51 -0400

    Update environment variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "secret_string" {
   type        = string
   description = "Secret String for AWS Secrets Manager"
 }
+
+variable "secret_key_base" {
+  type        = string
+  description = "Secret Key base for Mind The Gapp Pheonix Application"
+}
