2e32801a2bb7b78294dcc114cda36bc9ea1e1b27
Naomi Dennis <naomi@8thlight.com>
2020-03-23 11:05:27 -0400

    Add release tasks

---
+++ lib/mind_the_gapp/release.ex
@@ -0,0 +1,17 @@
+defmodule MindTheGapp.Release do
+  @app :mind_the_gapp
+  def migrate do
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
