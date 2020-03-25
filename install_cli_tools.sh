4660f7e35e2e24e5f3cc3b76363c84b67f673e0e
Naomi Dennis <naomi@8thlight.com>
2020-03-25 18:16:44 -0400

    Set destination for local aws commmand

--- install_cli_tools.sh
+++ install_cli_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip ./aws_local
+unzip awscliv2.zip -d ./aws_local
 sudo ./aws_local/aws/install -b ./aws_cmd
 export tf_version=0.12.20 tf_init_cli_options="-input=false" tf_validation_cli_options="" tf_plan_cli_options="-lock=false -input=false" tf_apply_cli_options="-auto-approve -input=false"
 wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
