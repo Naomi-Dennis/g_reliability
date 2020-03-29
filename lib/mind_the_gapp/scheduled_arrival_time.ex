9bc33682b86aaa8cfb9636764f383b129d1ef83b
Naomi Dennis <naomi@8thlight.com>
2020-03-29 11:57:55 -0400

    Add scheduled arrival times

---
+++ lib/mind_the_gapp/scheduled_arrival_time.ex
@@ -0,0 +1,9 @@
+defmodule MindTheGapp.ScheduledArrivalTime do
+  use Ecto.Schema
+  alias MindTheGapp.Repo
+  schema "scheduled_arrival_times" do
+    field :trip_id, :string
+    field :arrival_time, :string
+    field :stop_id, :string
+  end
+end
